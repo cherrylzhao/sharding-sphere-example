f6ef76fc37e21add56f07b6e86685ad2ce671e82
cherrylzhao <zhaojunv3@126.com>
2019-02-25 14:21:43 +0800

    add prepare statement perf case.

--- sharding-jdbc-example/spring-boot-nodep-example/spring-boot-nodep-jpa-example/src/main/java/io/shardingsphere/example/spring/boot/jpa/nodep/JDBCPerformanceController.java
+++ sharding-jdbc-example/spring-boot-nodep-example/spring-boot-nodep-jpa-example/src/main/java/io/shardingsphere/example/spring/boot/jpa/nodep/JDBCPerformanceController.java
@@ -17,11 +17,10 @@
 
 package io.shardingsphere.example.spring.boot.jpa.nodep;
 
-import io.shardingsphere.core.exception.ShardingException;
 import io.shardingsphere.example.repository.jpa.service.SpringEntityTransactionService;
+import org.apache.shardingsphere.core.exception.ShardingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
